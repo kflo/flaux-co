cf6dcef3defef654ee7be99eeba1fe644426e8cf
Kevin Florian <kevin.florian@bullhorn.com>
2025-06-11 16:48:40 -0400

    init commit

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected title = 'flaux-co';
+
+}
