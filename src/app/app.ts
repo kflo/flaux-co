90e624d36d2e0776a215be92dfa35e013e621438
Kevin Florian <kevin.florian@bullhorn.com>
2025-06-19 11:07:47 -0400

    bg gradient work

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { BgComponent } from '../bg/bg.component';
 
 @Component({
   selector: 'app-root',
+  imports: [BgComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
