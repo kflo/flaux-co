a06da029c011fe1d2c4aac99d76db7d0f358f0b9
Kevin Florian <kflo@outlook.com>
2025-08-16 15:27:47 -0400

    lottie list scroller v1

--- projects/flaux-co/src/app/pages/home/home.page.ts
+++ projects/flaux-co/src/app/pages/home/home.page.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import lottie from 'lottie-web';
 
 @Component({
   selector: 'flaux-home',
   standalone: true,
   templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss']
+  styleUrls: ['./home.page.scss'],
 })
-export class FlauxHomePage {}
+export class FlauxHomePage implements AfterViewInit {
+  @ViewChild('lottieContainer', { static: true }) lottieContainer!: ElementRef;
+
+  ngAfterViewInit(): void {
+    lottie.loadAnimation({
+      container: this.lottieContainer.nativeElement, // reference to the div
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: 'assets/img/list-scroll-wide.json',
+    });
+  }
+}
