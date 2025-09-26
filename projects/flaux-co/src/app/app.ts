2d499ee1d905dfbc520c18fee84f397f7d16ecc1
Kevin Florian <kflo@outlook.com>
2025-09-26 13:51:33 -0400

    footer update

--- projects/flaux-co/src/app/app.ts
+++ projects/flaux-co/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '@components/navbar/navbar.component';
 import { DropdownMenuComponent } from '@components/dropdown-menu/dropdown-menu.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from '@components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, DropdownMenuComponent, RouterModule],
+  imports: [NavbarComponent, DropdownMenuComponent, FooterComponent, RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
