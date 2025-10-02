b960cd7af58927a3cb5cde9b614286951ff6f968
Kevin Florian <kflo@outlook.com>
2025-10-01 20:47:07 -0400

    bug fix

--- projects/flaux-co/src/app/pages/home/home.page.ts
+++ projects/flaux-co/src/app/pages/home/home.page.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { FeaturesSectionComponent } from './features-section/features-section.component';
 import { CtaSectionComponent } from './cta-section/cta-section.component';
 import { Features2SectionComponent } from './features2-section/features2-section.component';
+import {AllInOneSectionComponent} from './all-in-one-section/all-in-one-section.component';
 
 @Component({
-  selector: 'flaux-home',
-  standalone: true,
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
-  imports: [
-    HeroSectionComponent,
-    FeaturesSectionComponent,
-    Features2SectionComponent,
-    CtaSectionComponent,
-  ],
+	selector: 'flaux-home',
+	standalone: true,
+	templateUrl: './home.page.html',
+	styleUrls: ['./home.page.scss'],
+	imports: [
+		HeroSectionComponent,
+		AllInOneSectionComponent,
+		Features2SectionComponent,
+		CtaSectionComponent,
+	],
 })
 export class FlauxHomePage {}
