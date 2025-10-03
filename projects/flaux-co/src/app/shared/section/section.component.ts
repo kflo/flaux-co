2d4f9d064ee4e0c6861cde33ba2389a274f74b81
Kevin Florian <kflo@outlook.com>
2025-10-02 20:31:20 -0400

    section update

--- projects/flaux-co/src/app/shared/section/section.component.ts
+++ projects/flaux-co/src/app/shared/section/section.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, Input } from '@angular/core';
 
 @Component({
-  selector: 'flaux-section',
-  standalone: true,
-  templateUrl: './section.component.html',
-  styleUrl: './section.component.scss'
+	selector: 'flaux-section',
+	standalone: true,
+	templateUrl: './section.component.html',
+	styleUrl: './section.component.scss'
 })
 export class FlauxSectionComponent {
+
+	@Input()
+	@HostBinding('style.height')
+		height: string = '50vh';
+
+	@Input()
+	@HostBinding('style.padding-inline')
+		inlinePadding: string = '5vw';
 
 }
