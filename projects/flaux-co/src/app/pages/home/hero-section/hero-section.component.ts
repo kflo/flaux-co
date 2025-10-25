9bf35ed09f46a229149e6573657bd382ed2cffe5
Kevin Florian <kflo@outlook.com>
2025-10-25 13:26:44 -0400

    feat: Update home hero section
    - Update hero-section.component.html, .scss, and .ts with new content and styling

--- projects/flaux-co/src/app/pages/home/hero-section/hero-section.component.ts
+++ projects/flaux-co/src/app/pages/home/hero-section/hero-section.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FlauxFnComponent } from '@app/shared/fn/fn.component';
 import {HlsComponent} from '@app/shared/hls/hls.component';
 import {environment} from '../../../../../environments/environment';
+import {FlauxSectionComponent} from '@app/shared/section/section.component';
 
 @Component({
 	selector: 'hero-section',
 	standalone: true,
-	imports: [HlsComponent, FlauxFnComponent],
+	imports: [HlsComponent, FlauxFnComponent, FlauxSectionComponent],
 	templateUrl: './hero-section.component.html',
 	styleUrls: ['./hero-section.component.scss'],
 })
