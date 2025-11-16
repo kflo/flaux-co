7674989ccbd2058232e1fca3413477535cc0dd35
Kevin Florian <kflo@outlook.com>
2025-11-15 23:28:11 -0500

    remove unused secret

--- flaux-functions/src/configs/vendasta.ts
+++ flaux-functions/src/configs/vendasta.ts
@@ -1,10 +1,11 @@
-import { defineString, defineSecret } from "firebase-functions/params";
-
+import { defineString } from "firebase-functions/params";
 /**
  * Vendasta OAuth configuration
  */
 export const VENDASTA_CLIENT_ID = defineString("VENDASTA_CLIENT_ID");
-export const VENDASTA_REDIRECT_URI = defineString("VENDASTA_REDIRECT_URI", { default: "https://flaux-site-dev.web.app/vendastaCallback" });
+export const VENDASTA_REDIRECT_URI = defineString("VENDASTA_REDIRECT_URI", {
+	default: "https://flaux-site-dev.web.app/vendastaCallback",
+});
 export const APP_BASE_URL = defineString("APP_BASE_URL", { default: "https://flaux-site-dev.web.app" });
 export const VENDASTA_API_BASE_URL = defineString("VENDASTA_API_BASE_URL", { default: "https://prod.apigateway.co/" });
 
@@ -12,7 +13,7 @@ export function getVendastaConfig() {
 	return {
 		vendasta: {
 			clientId: VENDASTA_CLIENT_ID.value(),
-			clientSecret: VENDASTA_CLIENT_SECRET.value(),
+			// clientSecret: VENDASTA_CLIENT_SECRET.value(),
 			redirectUri: VENDASTA_REDIRECT_URI.value(),
 			authUrl: "https://sso.vendasta.com/oauth/authorize",
 			tokenUrl: "https://sso.vendasta.com/oauth/token",
